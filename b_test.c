a83acf391f98c2e5e0723308b908ed7937d03b15
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2018-09-28 14:36:21 +0800

    add typedef struct for static test

--- b_test.c
+++ b_test.c
@@ -2,10 +2,19 @@
 
 #include <stdio.h>
 
+typedef struct
+{
+   int u16B; 
+   char aB[10];
+}TypeDef;
+
+static TypeDef typedef_ = {10, "bbb"};
+
 static int test_value = 1;
 
 void get_b_test_value(void)
 {
+    printf("%s, %d, %s, %p \r\n", __FUNCTION__, typedef_.u16B, typedef_.aB, &typedef_);
 
     printf("%s, test_value=%d, test_value=%p\r\n", __FUNCTION__, test_value++, &test_value);    
 }
